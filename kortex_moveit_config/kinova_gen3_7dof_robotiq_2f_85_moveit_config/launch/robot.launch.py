59cc7ee38cd48db20c1a420d823018b05abd1ea3
cobot <cobot@robot>
2025-09-29 23:13:13 -0500

    fix race condition in kinova_gen3_7dof_robotiq_2f_85_moveit_config/launch/robot.launch.py

--- kortex_moveit_config/kinova_gen3_7dof_robotiq_2f_85_moveit_config/launch/robot.launch.py
+++ kortex_moveit_config/kinova_gen3_7dof_robotiq_2f_85_moveit_config/launch/robot.launch.py
@@ -109,9 +109,13 @@ def launch_setup(context, *args, **kwargs):
     ros2_control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[ros2_controllers_path],
+        parameters=[
+        moveit_config.robot_description,   # <-- add this line
+        ros2_controllers_path,
+            {"update_rate": 1000},
+        ],
         remappings=[
-            ("/controller_manager/robot_description", "/robot_description"),
+            ("/controller_manager/robot_description", "/robot_description"),  # you can keep this
         ],
         output="both",
     )
